fcb4f8baa9d6b48e0e28c155305fb478b8c97a53
GrooveStomp <aaron.oman@gmail.com>
2013-05-15 16:45:42 -0700

    foo

--- env.rb
+++ env.rb
@@ -1,3 +1,3 @@
-ENV['OAUTH2_CLIENT_ID']           = ""
-ENV['OAUTH2_CLIENT_SECRET']       = ""
+ENV['OAUTH2_CLIENT_ID']           = "8fcbf9759e03460aeac61995ffde69a29e40db0e65a52f2e623e42a1f18bd51c"
+ENV['OAUTH2_CLIENT_SECRET']       = "e035ed79816cd81fdc452fe8c7b45575ff5e7b079b99dedaee586a70741bf503"
 ENV['OAUTH2_CLIENT_REDIRECT_URI'] = "http://oauth-demo.heroku.com/callback"
